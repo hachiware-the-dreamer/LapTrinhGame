6769bbd89069251ee7d6fda938d08927627fd8f3
steamingbuns <anh.nguyenthek23@hcmut.edu.vn>
2026-02-27 09:09:47 +0700

    particles fix

--- BTL2/Tank Battle/Assets/Scripts/BulletPhysics.cs
+++ BTL2/Tank Battle/Assets/Scripts/BulletPhysics.cs
@@ -53,11 +53,11 @@ public class BulletPhysics : MonoBehaviour
             if (AudioManager.Instance != null)
             {
                 AudioManager.Instance.PlayBounce();
-            if (bounceCount > maxBounces)
-            {
-                Destroy(gameObject);
-                return;
-            }  
+                if (bounceCount > maxBounces)
+                {
+                    Destroy(gameObject);
+                    return;
+                }  
             }
             Vector2 n = hit.normal;
             Vector2 vOld = velocity;
